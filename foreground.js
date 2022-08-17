d2acec99fda25e93ce78c4790a8f3ffbe96fbf4a
Marc Sabatella <marc@outsideshore.com>
2022-08-16 21:27:40 -0600

    use headings

--- foreground.js
+++ foreground.js
@@ -15,6 +15,7 @@ async function getPages (chapter) {
   return pages;
 }
 
+// show main headings as subheadings; subheadings as lists
 async function getHeadings (page) {
   //console.log("getHeadings");
   const fetchInit = { "cache": "default", };
@@ -24,7 +25,53 @@ async function getHeadings (page) {
   let x = d.parseFromString(t, "text/html");
   let content = x.querySelector(".content-wrapper");
   let raw = content.querySelectorAll("h2, h3, h4, h5");
-  var headings = document.createElement("ul");
+  var headings = document.createElement("div");
+  var top = headings;
+  var level = "H2";
+  for (const h of raw) {
+    let eType =(h.nodeName == "H2") ? "h4" : "li";
+    let i = document.createElement(eType);
+    let anchor = h.querySelector("a");
+    if (anchor) {
+      let ahref = anchor.getAttribute("href");
+      let a = document.createElement("a");
+      a.href = page + ahref;
+      a.innerText = h.innerText;
+      i.innerHTML = a.outerHTML;
+      //console.log("anchor found " + i.innerHTML);
+    } else {
+      i.innerText = h.innerText;
+      //console.log("no anchor found " + i.innerHTML);
+    }
+    if (h.nodeName > level) {
+      let inner = document.createElement("ul");
+      headings.append(inner);
+      headings = inner;
+    } else if (h.nodeName < level) {
+      let n = level.substring(1) - h.nodeName.substring(1);
+      //console.log(level + " - " + h.nodeName + " = " + n);
+      for (let ii = 0; ii < n; ++ii) {
+        headings = headings.parentNode;
+      }
+    }
+    level = h.nodeName;
+    headings.append(i);
+    //console.log(h.nodeName + " " + i.innerText);
+  }
+  return top;
+}
+
+// show all headings as lists
+async function getHeadingsList (page) {
+  //console.log("getHeadings");
+  const fetchInit = { "cache": "default", };
+  let r = await fetch(page, fetchInit);
+  let t = await r.text();
+  let d = new DOMParser;
+  let x = d.parseFromString(t, "text/html");
+  let content = x.querySelector(".content-wrapper");
+  let raw = content.querySelectorAll("h2, h3, h4, h5");
+  var headings = document.createElement("div");
   var top = headings;
   var level = "H1";
   for (const h of raw) {
@@ -46,7 +93,11 @@ async function getHeadings (page) {
       headings.append(inner);
       headings = inner;
     } else if (h.nodeName < level) {
-      headings = headings.parentNode;
+      let n = level.substring(1) - h.nodeName.substring(1);
+      //console.log(level + " - " + h.nodeName + " = " + n);
+      for (let ii = 0; ii < n; ++ii) {
+        headings = headings.parentNode;
+      }
     }
     level = h.nodeName;
     headings.append(i);
@@ -64,9 +115,9 @@ async function buildTOC (url, toc) {
     toc.append(n);
     let pages = await getPages(c.href);
     if (pages) {
-      let l = document.createElement("ul");
+      let l = document.createElement("div");
       for (const p of pages) {
-        let n = document.createElement("li");
+        let n = document.createElement("h3");
         let a = document.createElement("a");
         a.innerText = p.innerText;
         a.href = p.href;
@@ -94,7 +145,7 @@ async function tocNew () {
     let ss = doc.createElement("style");
     ss.type = "text/css";
     doc.head.append(ss);
-    ss.sheet.insertRule("h2, li, a { color: lightgray; }", 0);
+    ss.sheet.insertRule("h2, h3, h4, li, a { color: lightgray; }", 0);
     await buildTOC(document, div);
     ss.sheet.deleteRule(0);
     let oh1 = document.querySelector("h1");
